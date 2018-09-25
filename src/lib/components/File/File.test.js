0d89e7b0b02a2c33c84c47621db977af4e435bca
opalerm <oscarpalerm@obitur.com>
2018-09-25 17:52:05 +0200

    Adds File Input snapshot

--- src/lib/components/File/File.test.js
+++ src/lib/components/File/File.test.js
@@ -5,7 +5,7 @@ import File from "./File";
 describe("Testing File input component", () => {
   it("matches snapshot", () => {
     const snap = renderer.create(
-      <File onLoadImage={result => console.log(`Loaded image ${result}`)} />
+      <File onLoadFile={result => console.log(`Loaded image ${result}`)} />
     );
 
     const tree = snap.toJSON();
