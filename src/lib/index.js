e8276d870c3c06e42dedf2a9085a5e25ad731209
opalerm <oscarpalerm@obitur.com>
2018-11-06 18:21:04 +0100

    Exports card

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,5 +2,6 @@ import Button from "./components/Button";
 import Text from "./components/Text";
 import Select from "./components/Select";
 import File from "./components/File";
+import Card from './components/Card';
 
-export { Button, Text, File, Select };
+export { Button, Text, File, Select, Card };
